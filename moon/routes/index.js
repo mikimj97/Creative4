976ab47f1c46b5a50955224842736cf2173ac1c5
mikimj97 <mikimj97@gmail.com>
2018-11-01 02:23:52 +0000

    Working API

--- moon/routes/index.js
+++ moon/routes/index.js
@@ -7,23 +7,25 @@ router.get('/', function(req, res, next) {
     res.sendFile('moon.html', { root: 'public' });
 });
 
-router.get('/moonByDate', function(req, res, next) { //Only finds the moon phase by the date
-    var url = "http://api.usno.navy.mil/moon/phase?date=";
-    var endUrl = "&nump=2";
+router.get('/getMoon', function(req, res, next) { //Dat&location
+    var url = "http://api.usno.navy.mil/rstt/oneday?date=";
+    //var endUrl = "&nump=2";
     console.log("REQ: " + req);
     console.log("query ", req.query);
     url += req.query['q'];
-    url += endUrl;
+    url += "&loc=";
+    url += req.query['loc'];
+    //url += endUrl;
     console.log("URL: " + url);
     request(url).pipe(res);
 });
 
-router.get('/moonByLocation', function(req, res, next) { //Will find the moon phase by the location? If I can find one
+/*router.get('/moonByLocation', function(req, res, next) { //Will find the moon phase by the location? If I can find one
 
     var url = "https://api.aerisapi.com/sunmoon/moonphases/";
     var endUrl = "?client_id=Fw9aMpcBNd2NsOomuU2qn&client_secret=9HHCVnQtpYWgaIl32T7kJHudrlwAwylrUrvv1zw5";
     request(url).pipe(res);
-});
+});*/
 
 //https://api.aerisapi.com/sunmoon/moonphases/burke,va?client_id=Fw9aMpcBNd2NsOomuU2qn&client_secret=9HHCVnQtpYWgaIl32T7kJHudrlwAwylrUrvv1zw5
 // http://api.usno.navy.mil/moon/phase?date=10/31/2018&nump=2
