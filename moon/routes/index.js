22a634faad43934959f579dd8b60bbabe5a6cb4f
Douglas Uriona <duriona@gmail.com>
2018-11-01 21:00:34 +0000

    Added pictures with each phase and cleaned up the logic better

--- moon/routes/index.js
+++ moon/routes/index.js
@@ -9,23 +9,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/getMoon', function(req, res, next) { //Dat&location
     var url = "http://api.usno.navy.mil/rstt/oneday?date=";
-    //var endUrl = "&nump=2";
     console.log("REQ: " + req);
     console.log("query ", req.query);
     url += req.query['q'];
     url += "&loc=";
     url += req.query['loc'];
-    //url += endUrl;
     console.log("URL: " + url);
     request(url).pipe(res);
 });
-
-/*router.get('/moonByLocation', function(req, res, next) { //Will find the moon phase by the location? If I can find one
-
-    var url = "https://api.aerisapi.com/sunmoon/moonphases/";
-    var endUrl = "?client_id=Fw9aMpcBNd2NsOomuU2qn&client_secret=9HHCVnQtpYWgaIl32T7kJHudrlwAwylrUrvv1zw5";
-    request(url).pipe(res);
-});*/
 
 //https://api.aerisapi.com/sunmoon/moonphases/burke,va?client_id=Fw9aMpcBNd2NsOomuU2qn&client_secret=9HHCVnQtpYWgaIl32T7kJHudrlwAwylrUrvv1zw5
 // http://api.usno.navy.mil/moon/phase?date=10/31/2018&nump=2
